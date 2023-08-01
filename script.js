95c9c0832acdac994163adbdc468f28f8d3b547b
Vihaan-exe <129576832+Vihaan-exe@users.noreply.github.com>
2023-08-01 12:44:55 +0530

    files added

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any interactive elements or functionality here (if needed)
+// For this basic example, JavaScript is not necessary
+
+// You can add a JavaScript-based form validation or interactivity for your projects, etc.
+// This is a basic template that you can expand upon depending on your needs.
